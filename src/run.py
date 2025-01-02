0122b4c7fb16baf7d3b918a1287e64b1866c0b90
wasabina67 <wasabina67@gmail.com>
2025-01-02 08:08:25 +0000

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -3,8 +3,13 @@ import irc.bot
 
 class Bot(irc.bot.SingleServerIRCBot):
 
-    def __init__(self):
-        pass
+    def __init__(self, server="ngircd", port=6667, nickname="bot", channel="#bot"):
+        super().__init__(server_list=[(server, port)], nickname=nickname, realname=nickname)
+        self.nickname = nickname
+        self.channel = channel
+
+    def on_welcome(self, connection, event):
+        connection.join(self.channel)
 
 
 def main():
